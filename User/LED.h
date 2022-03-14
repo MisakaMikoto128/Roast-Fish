2ecfc77fef9caf53a52ac9ec68c5ff39f4e0c0bc
MisakaMikoto128 <liuyuanlins@outlook.com>
2022-03-14 10:38:31 +0800

    LED测试通过

--- User/LED.h
+++ User/LED.h
@@ -2,12 +2,12 @@
 #define LED_H
 #include "gpio.h"
 
-#define LED_Pin GPIO_Pin_13
+#define LED_Pin GPIO_PIN_13
 #define LED_GPIO_Port GPIOB
 #define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
 
 void LED_Init(void);
-#define LED_ON() HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET)
-#define LED_OFF() HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET)
+#define LED_ON() HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET)
+#define LED_OFF() HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET)
 
 #endif // LED_H
